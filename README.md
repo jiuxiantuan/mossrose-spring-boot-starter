f368c3cae1166cbc6476e0c7266293006d2538ab
yuxuanwang <crnlmcn@gmail.com>
2018-03-09 17:52:40 +0800

    add mossrose ui

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring boot starter for mossrose
+<hr>
+
+```
+		<dependency>
+			<groupId>com.jiuxian</groupId>
+			<artifactId>mossrose-spring-boot-starter</artifactId>
+			<version>1.0.0-RELEASE</version>
+		</dependency>
+```
--- src/main/java/com/jiuxian/boot/mossrose/autoconfigure/MossroseAutoConfiguration.java
+++ src/main/java/com/jiuxian/boot/mossrose/autoconfigure/MossroseAutoConfiguration.java
@@ -3,12 +3,18 @@ package com.jiuxian.boot.mossrose.autoconfigure;
 import com.jiuxian.mossrose.MossroseProcess;
 import com.jiuxian.mossrose.config.MossroseConfig;
 import com.jiuxian.mossrose.spring.SpringContextHolder;
+import com.jiuxian.mossrose.ui.RestMossroseUI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @ConditionalOnClass(MossroseProperties.class)
@@ -22,16 +28,56 @@ public class MossroseAutoConfiguration {
     private ApplicationContext applicationContext;
 
     @Bean
-    public MossroseConfig.Cluster mossroseConfig() {
+    public MossroseProcess mossroseProcess() {
+        MossroseConfig config = new MossroseConfig();
+
         MossroseConfig.Cluster cluster = new MossroseConfig.Cluster();
-        if(mossroseProperties.getName() != null) {
+        if (mossroseProperties.getName() != null) {
             cluster.setName(mossroseProperties.getName());
         }
-        if(mossroseProperties.getDiscoveryZk() != null) {
+        if (mossroseProperties.getDiscoveryZk() != null) {
             cluster.setDiscoveryZk(mossroseProperties.getDiscoveryZk());
         }
+        if (mossroseProperties.getRunOnMaster() != null) {
+            cluster.setRunOnMaster(mossroseProperties.getRunOnMaster());
+        }
+        if (mossroseProperties.getPort() != null) {
+            cluster.setPort(mossroseProperties.getPort());
+        }
+        if (mossroseProperties.getLoadBalancingMode() != null) {
+            cluster.setLoadBalancingMode(MossroseConfig.Cluster.LoadBalancingMode.valueOf(mossroseProperties.getLoadBalancingMode()));
+        }
 
-        return cluster;
+        config.setCluster(cluster);
+
+        List<MossroseConfig.JobMeta> jobs = new ArrayList<>();
+        final Map<String, Object> jobBeans = applicationContext.getBeansWithAnnotation(Job.class);
+        if (jobBeans == null || jobBeans.isEmpty()) {
+            throw new IllegalStateException("No job bean.");
+        }
+        for (Map.Entry<String, Object> entry : jobBeans.entrySet()) {
+            MossroseConfig.JobMeta job = new MossroseConfig.JobMeta();
+            job.setJobBeanName(entry.getKey());
+
+            final Job jobAnnotation = entry.getValue().getClass().getAnnotation(Job.class);
+            job.setId(jobAnnotation.id());
+            job.setGroup(jobAnnotation.group());
+            job.setCron(jobAnnotation.cron());
+            job.setDescription(jobAnnotation.description());
+
+            jobs.add(job);
+        }
+        config.setJobs(jobs);
+
+        return new MossroseProcess(config);
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "mossrose", name = "enable-ui", havingValue = "true")
+    public RestMossroseUI mossroseUI(MossroseProcess mossroseProcess) {
+        final Integer uiPort = mossroseProperties.getUiPort();
+        int port = uiPort != null ? uiPort : 7758;
+        return new RestMossroseUI(mossroseProcess, port);
     }
 
     @Bean
